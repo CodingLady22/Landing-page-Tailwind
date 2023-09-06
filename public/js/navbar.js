9cdb1e3b628a507c1f90eca2780fbaa488d12791
Maye Jesuorobo <nayawes@yahoo.com>
2023-09-06 16:44:44 +0200

    added the functionalities to activate and deactivate the dropdown menus. Also edited the code from Mayanwolfe

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,3 +1,67 @@
+// Hamburger menu toggle
+let hamburger = document.getElementById("hamburger");
+let links = document.getElementById("links");
+let main = document.querySelector("main");
+
+hamburger.addEventListener("click", toggleHamburgerMenu);
+
+function toggleHamburgerMenu() {
+  hamburger.classList.toggle("active");
+  links.classList.toggle("hidden");
+}
+
+// Close the dropdown if the user clicks outside of it
+document.addEventListener("click", function (event) {
+  if (!hamburger.contains(event.target) && !links.contains(event.target)) {
+    hamburger.classList.remove("active");
+    links.classList.add("hidden");
+  }
+});
+
+// Recipes dropdown
+const recipesButton = document.getElementById("recipesButton");
+const recipeDropdown = document.getElementById("recipeDropdown");
+const contentBelowDropdown = document.getElementById("contentBelowDropdown");
+
+recipesButton.addEventListener("click", function (event) {
+  event.preventDefault(); // Prevent the click event from triggering form submission
+  recipeDropdown.classList.toggle("hidden"); // Toggle visibility of the menu links
+//   contentBelowDropdown.style.marginTop = recipeDropdown.classList.contains("hidden") ? "0" : recipeDropdown.offsetHeight + "px";
+});
+
+ // Close the dropdown if the user clicks outside of it
+document.addEventListener("click", function (event) {
+    if (!recipesButton.contains(event.target) && !recipeDropdown.contains(event.target)) {
+      recipeDropdown.classList.add("hidden");
+    }
+});
+
+/*
+//*! Start of js for second dropdown menu
+let dropdown = document.getElementById("recipesButton")
+
+dropdown.addEventListener("click", toggleDropdown)
+
+function toggleDropdown() {
+	const recipesButton = document.querySelector("#recipesButton #recipeDropdown");
+	recipesButton.classList.toggle("hidden")
+}
+
+ // Close the dropdown if the user clicks outside of it
+document.addEventListener("click", function (event) {
+    if (!recipesButton.contains(event.target) && !recipeDropdown.contains(event.target)) {
+      recipeDropdown.classList.add("hidden");
+    }
+});
+
+//*! End of js for second dropdown menu
+*/
+
+
+
+
+/*
+//* From Mayanwolfe
 // This is for the navbar only.
 let hamburger = document.getElementById("hamburger");
 let links = document.getElementById("links");
@@ -37,3 +101,22 @@ function changeMenuState() {
 function removeActive() {
 	hamburger.classList.remove("active");
 }
+
+//* End from Mayanwolfe
+
+// Recipes dropdown
+const recipesButton = document.getElementById("recipesButton");
+const recipeDropdown = document.getElementById("recipeDropdown");
+
+recipesButton.addEventListener("click", (event) => {
+	event.preventDefault(); // Prevent the click event from triggering form submission
+    recipeDropdown.classList.toggle("hidden"); // Toggle visibility of the menu links
+});
+
+ // Close the dropdown if the user clicks outside of it
+document.addEventListener("click", function (event) {
+    if (!recipesButton.contains(event.target) && !recipeDropdown.contains(event.target)) {
+      recipeDropdown.classList.add("hidden");
+    }
+});
+*/
